6d2c7da845ab0b8e3e5b3da5f368f79086716bae
Geraldalex <alex1989chekan@gmail.com>
2023-07-24 21:45:58 +0300

    day5

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import "./scss/app.scss"
 import {Home, Cart} from './pages';
 import {Routes,Route } from "react-router-dom";
 import { Header } from "./components";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+fetch('http://localhost:3000/db.json').then(resp => resp.json()).then(json => setPizzas(json.pizzas))
+   } , [])
+
+
   return (
     <div className="wrapper">
 <Header/>
       <div className="content">
 
       <Routes>
-          <Route path="/" element={   <Home/> }/>
+          <Route path="/" element={   <Home pizzas={pizzas}/> }/>
         <Route path="/cart" element={<Cart/>} />
       </Routes>
       
