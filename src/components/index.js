7037e45b61c0aab852f798941511089815c9618a
Geraldalex <alex1989chekan@gmail.com>
2023-08-23 16:17:56 +0300

    ok

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export {default as Header} from './Header'
 export {default as SortPopup} from './SortPopup'
 export {default as PizzaBlock} from './PizzaBlock'
 export {default as LoadingBlock} from './LoadingBlock'
+export {default as CartItem} from './CartItem'
 
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,21 +3,27 @@ const initialState = {
     totalPrice: 0,
     totalCount:0
   }
+
+  const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0)
   
   const cart = (state = initialState, action) => {
   
   switch(action.type){
     case 'ADD_PIZZA_CART':
 
+    const currentPizzaItems = !state.items[action.payload.id]
+    ? [action.payload] 
+    :  [...state.items[action.payload.id], action.payload]
+
     const newItems = {
       ...state.items,
-      [action.payload.id]: !state.items[action.payload.id]
-       ? [action.payload] 
-       :  [...state.items[action.payload.id], action.payload]
+      [action.payload.id]: currentPizzaItems,
+      totalPrice: getTotalPrice(currentPizzaItems)
     }
 
     const allPizzas = [].concat(...Object.values(newItems))
-    const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0)
+    console.log(allPizzas)
+    const totalPrice = getTotalPrice(allPizzas)
 
 
       return {
